d79ad3533ae89b1235a28d916c66d87fb2d550e8
NuranTerlan <58745191+NuranTerlan@users.noreply.github.com>
2020-06-14 14:48:38 +0400

    initial

---
+++ stories/2-Icons.stories.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import Button from "../components/button";
+import * as Icons from "../components/icons";
+
+export default {
+  title: "Icons",
+};
+
+export const Icon = () => (
+  <div className="icons">
+    <Icons.ArrowBottom/>
+    <Icons.Bookmarks/>
+    <Icons.BookmarksFill/>
+    <Icons.Calendar/>
+    <Icons.Comment/>
+    <Icons.Emoji/>
+    <Icons.Explore/>
+    <Icons.ExploreFill/>
+    <Icons.Gif/>
+    <Icons.Home/>
+    <Icons.HomeFill/>
+    <Icons.Image/>
+    <Icons.Like/>
+    <Icons.Lists/>
+    <Icons.ListsFill/>
+    <Icons.Messages/>
+    <Icons.MessagesFill/>
+    <Icons.More/>
+    <Icons.Notifications/>
+    <Icons.NotificationsFill/>
+    <Icons.Profile/>
+    <Icons.ProfileFill/>
+    <Icons.Quiz/>
+    <Icons.Retweet/>
+    <Icons.Search/>
+    <Icons.Settings/>
+    <Icons.Share/>
+    <Icons.Tweet/>
+    <Icons.TweetOrder/>
+    <Icons.Twitter/>
+  </div>
+);
