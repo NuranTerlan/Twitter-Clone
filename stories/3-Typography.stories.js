2cb03f1dee572a3d30ee73e5a8be5920e6c71bdb
NuranTerlan <58745191+NuranTerlan@users.noreply.github.com>
2020-06-15 18:30:47 +0400

    typography section is added to storybook

---
+++ stories/3-Typography.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import TextTitle from "./../components/text-title";
+
+export default {
+  title: "Typography",
+};
+
+export const Title = () => (
+  <div>
+    <TextTitle>Hello World !</TextTitle>
+    <TextTitle bold={false}>Hello World !</TextTitle>
+  </div>
+);
