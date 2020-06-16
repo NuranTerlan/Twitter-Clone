fd9223615a8a69e08c7622bb098322215730677c
NuranTerlan <58745191+NuranTerlan@users.noreply.github.com>
2020-06-17 03:16:17 +0400

    Create col-sidebar.js

---
+++ components/col-sidebar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import cn from "classnames";
+
+import styles from "./col-sidebar.module.css";
+
+import Navigation from "./navigation";
+import ThemeButton from "./theme-button";
+import ProfileBox from "./profile-box";
+import { Tweet } from "./icons";
+
+const SideBar = ({ flat }) => {
+  return (
+    <div className={cn(styles.sidebar)}>
+      <Navigation flat={flat} selectedKey="home" />
+      <div className={styles.tweet}>
+        <ThemeButton full={!flat} tall>
+          {flat ? <Tweet /> : "Tweet"}
+        </ThemeButton>
+      </div>
+      <div className={styles.profile}>
+        <ProfileBox flat={flat} />
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
