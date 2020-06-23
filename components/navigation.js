f7074ece224827b49de3fe0ceb682f1605be8245
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-06-24 01:01:36 +0400

    icons carried to constants for navigation ...

--- components/navigation.js
+++ components/navigation.js
@@ -1,101 +1,34 @@
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { MENU } from "../constants";
+import styles from "./navigation.module.css";
 
 import NavButton from "./nav-button";
 import TextTitle from "./text-title";
-import {
-  Twitter,
-  Home,
-  Explore,
-  Notifications,
-  Messages,
-  Bookmarks,
-  Lists,
-  Profile,
-  More,
-  HomeFill,
-  ExploreFill,
-  NotificationsFill,
-  MessagesFill,
-  BookmarksFill,
-  ListsFill,
-  ProfileFill,
-} from "./icons";
 
-const MENU = [
-  {
-    key: "twitter",
-    icon: <Twitter />,
-    title: "",
-  },
-  {
-    key: "home",
-    icon: <Home />,
-    iconSelected: <HomeFill />,
-    title: "Home",
-  },
-  {
-    key: "explore",
-    icon: <Explore />,
-    iconSelected: <ExploreFill />,
-    title: "Explore",
-  },
-  {
-    key: "notifications",
-    icon: <Notifications />,
-    iconSelected: <NotificationsFill />,
-    title: "Notifications",
-    notify: 17,
-  },
-  {
-    key: "messages",
-    icon: <Messages />,
-    iconSelected: <MessagesFill />,
-    title: "Messages",
-  },
-  {
-    key: "bookmarks",
-    icon: <Bookmarks />,
-    iconSelected: <BookmarksFill />,
-    title: "Bookmarks",
-  },
-  {
-    key: "lists",
-    icon: <Lists />,
-    iconSelected: <ListsFill />,
-    title: "Lists",
-  },
-  {
-    key: "profile",
-    icon: <Profile />,
-    iconSelected: <ProfileFill />,
-    title: "Profile",
-  },
-  {
-    key: "more",
-    icon: <More />,
-    title: "More",
-  },
-];
 
-import styles from "./navigation.module.css";
+const Navigation = ({ flat = false }) => {
+  const router = useRouter();
 
-const Navigation = ({ flat = false, selectedKey }) => {
   return (
     <nav className={styles.nav}>
       {MENU.map((menuItem) => {
         const showFullNav = !flat && menuItem.title.length > 0;
-        const selected = selectedKey === menuItem.key;
+        const selected = router.pathname === menuItem.path;
 
         return (
+          <Link key={menuItem.key} href={`${menuItem.path}`}>
             <NavButton
-            className={styles.item}
-            key={menuItem.key}
-            notify={menuItem.notify}
-            selected={selected}
-          >
-            {selected ? menuItem.iconSelected : menuItem.icon}
-            {showFullNav && <TextTitle>{menuItem.title}</TextTitle>}
-          </NavButton>
+              className={styles.item}
+              notify={menuItem.notify}
+              selected={selected}
+            >
+              {selected ? menuItem.iconSelected : menuItem.icon}
+              {showFullNav && <TextTitle>{menuItem.title}</TextTitle>}
+            </NavButton>
+          </Link>
         );
       })}
     </nav>
--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,74 @@
+import React from "react";
+import * as Icon from "../components/icons";
+
 export default {
   TABLET_SIZE: 980,
   DESKTOP_SIZE: 1270,
 };
+
+ export const MENU = [
+  {
+    key: "twitter",
+    path: "/home",
+    icon: <Icon.Twitter />,
+    iconSelected: <Icon.Twitter style={{ color: "var(--c-text-color)"}}/>,
+    title: "",
+  },
+  {
+    key: "home",
+    path: "/home",
+    icon: <Icon.Home />,
+    iconSelected: <Icon.HomeFill />,
+    title: "Home",
+  },
+  {
+    key: "explore",
+    path: "/explore",
+    icon: <Icon.Explore />,
+    iconSelected: <Icon.ExploreFill />,
+    title: "Explore",
+  },
+  {
+    key: "notifications",
+    path: "/notifications",
+    icon: <Icon.Notifications />,
+    iconSelected: <Icon.NotificationsFill />,
+    title: "Notifications",
+    notify: 17,
+  },
+  {
+    key: "messages",
+    path: "/messages",
+    icon: <Icon.Messages />,
+    iconSelected: <Icon.MessagesFill />,
+    title: "Messages",
+  },
+  {
+    key: "bookmarks",
+    path: "/bookmarks",
+    icon: <Icon.Bookmarks />,
+    iconSelected: <Icon.BookmarksFill />,
+    title: "Bookmarks",
+  },
+  {
+    key: "lists",
+    path: "/lists",
+    icon: <Icon.Lists />,
+    iconSelected: <Icon.ListsFill />,
+    title: "Lists",
+  },
+  {
+    key: "profile",
+    path: "/profile",
+    icon: <Icon.Profile />,
+    iconSelected: <Icon.ProfileFill />,
+    title: "Profile",
+  },
+  {
+    key: "more",
+    path: "/more",
+    icon: <Icon.More />,
+    iconSelected: <Icon.More />,
+    title: "More",
+  },
+];
