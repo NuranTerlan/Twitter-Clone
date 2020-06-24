0342c14626f0ead96532ae2d938c59a4fc99ed87
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-06-24 15:17:42 +0400

    layout problem for sidebar is solved ...

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,26 @@
 import React from "react";
 import cn from "classnames";
+import CONST from "../constants/index";
+import useWindowSize from "./../hooks/useWindowSize";
 
 import styles from "./layout.module.css";
 
+import SideBar from "./../components/col-sidebar";
+import Main from "./../components/col-main";
+import Extra from "./../components/col-extra";
+
 const Layout = ({ children }) => {
-  return <div className={cn(styles.layout)}>{children}</div>;
+  const size = useWindowSize();
+
+  return (
+    <div className={cn(styles.layout)}>
+      <SideBar flat={size.width < CONST.DESKTOP_SIZE}>Sidebar</SideBar>
+      <Main>
+        {children}
+      </Main>
+      {size.width > CONST.TABLET_SIZE && <Extra>Extra</Extra>}
+    </div>
+  );
 };
 
 export default Layout;
