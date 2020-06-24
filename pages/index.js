0832e7be2271c4418fa4e90a0031e48f7326ccd9
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-06-24 15:16:54 +0400

    redirection configuration when app is started is configured !

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Home from "./home";
+import { useRouter } from "next/router";
 
-import CONST from "../constants/index"
-import Layout from "../components/layout";
-import useWindowSize from "./../hooks/useWindowSize";
-import SideBar from './../components/col-sidebar';
-import Main from './../components/col-main';
-import Extra from './../components/col-extra';
+const FirstPage = () => {
+  const router = useRouter();
 
-function LayoutPage({ children }) {
-  const size = useWindowSize();
+  useEffect(() => {
+    if (router.pathname == "/") {
+      router.push("/home");
+    }
+  });
+  return null;
+};
 
-  return (
-    <Layout>
-      <SideBar flat={size.width < CONST.DESKTOP_SIZE}>Sidebar</SideBar>
-      <Main>
-        <div>Main.Size : {JSON.stringify(size)}</div>
-        {children}
-      </Main>
-      {size.width > CONST.TABLET_SIZE && <Extra>Extra</Extra>}
-    </Layout>
-  );
-}
-
-export default LayoutPage;
+export default FirstPage;
