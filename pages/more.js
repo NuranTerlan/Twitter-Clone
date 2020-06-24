e8328b8c153867c1ae379e1fccdf18e85b321222
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2020-06-24 22:33:15 +0400

    Update more.js

--- pages/more.js
+++ pages/more.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../components/layout";
+import ThemeOptions from "../components/theme-options";
 
 const More = () => {
   return (
     <Layout>
-      <h1>More page is here !</h1>
+      <h1>Choose theme that you want to see !</h1>
+      <ThemeOptions />
     </Layout>
   );
 };
